6c59b0e7d8f1fb8237b24b8195d66f29ce8b3e75
Flavio <flaviosestu@yahoo.fr>
2022-12-11 01:01:54 +0100

    Adding emails and exporting to csv

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from reader import Reader
 from chooser import Chooser
+import csv
 
 file = Reader("form.csv")
 file.clean_odd_classes()
 file.clean_odd_alone_sectors()
 chooser = Chooser(file.classes, file.alone_sectors, file.alone_students)
 result = chooser.assign_students()
-print(result)
+print(result, len(result.keys()))
+
+for sender in result.keys():
+    mail = file.data["Mail"][file.data["Nom"].index(sender.split()[0])]
+    result[sender] = (result[sender], mail)
+
+with open("result3121354655.csv", "w", newline="") as result_file:
+    writer = csv.writer(
+        result_file, delimiter=";", quotechar="|", quoting=csv.QUOTE_MINIMAL
+    )
+    writer.writerow(["Envoyeur", "Mail envoyeur", "Receveur"])
+    for student in result.keys():
+        writer.writerow([student, result[student][1], result[student][0]])
